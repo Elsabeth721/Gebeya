832f11bac48f865fa108057e54383555da431c11
Elsabeth721 <elsabethzeleke1000@gmail.com>
2025-01-11 13:20:54 +0300

    Modified the UI for sidebar

--- Frontend/PersonalExpenseTracker/src/components/Sidebar.jsx
+++ Frontend/PersonalExpenseTracker/src/components/Sidebar.jsx
@@ -1,33 +1,41 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom';
-import DarkModeToggle from './darkMode';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import DarkModeToggle from "./darkMode";
 
 const Sidebar = () => {
   return (
-    <div className='h-screen bg-slate-300 dark:bg-slate-900 w-64 text-gray-900 dark:text-gray-300 p-5'>
-      <h2 className='text-2xl font-bold mb-6'>Personal Expense Tracker</h2>
+    <div className="h-screen bg-slate-300 dark:bg-slate-900 w-64 text-gray-900 dark:text-gray-300 p-5">
+      <h2 className="text-2xl font-bold mb-6">Personal Expense Tracker</h2>
       <ul>
-        <li className='mb-4'>
-          <Link to='/' className='hover:text-gray-400'>Home</Link>
+        <li className="mb-4">
+          <Link to="/" className="hover:text-gray-400">
+            Home
+          </Link>
         </li>
-        <li className='mb-4'>
-          <Link to='/addExpense' className='hover:text-gray-400'>Add Expense</Link>
+        <li className="mb-4">
+          <Link to="/addExpense" className="hover:text-gray-400">
+            Add Expense
+          </Link>
         </li>
-        <li className='mb-4'>
-          <Link to='/viewExpense' className='hover:text-gray-400'>View Expense</Link>
+        <li className="mb-4">
+          <Link to="/viewExpense" className="hover:text-gray-400">
+            View Expense
+          </Link>
         </li>
-        <li className='mb-4'>
-          <Link to='/dashboard' className='hover:text-gray-400'>Dashboard</Link>
+        <li className="mb-4">
+          <Link to="/dashboard" className="hover:text-gray-400">
+            Dashboard
+          </Link>
         </li>
-        <li className='mb-4'>
-          <Link to='/filter' className='hover:text-gray-400'>Filter</Link>
+        <li className="mb-4">
+          <Link to="/filter" className="hover:text-gray-400">
+            Filter
+          </Link>
         </li>
       </ul>
       <DarkModeToggle />
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
-
-
+export default Sidebar;
