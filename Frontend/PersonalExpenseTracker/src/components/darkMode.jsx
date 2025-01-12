2dd36f4ce68406498a8c5094683415bd5f73da33
Elsabeth721 <elsabethzeleke1000@gmail.com>
2025-01-12 19:24:21 +0300

    modified code structure of the darkmode

--- Frontend/PersonalExpenseTracker/src/components/darkMode.jsx
+++ Frontend/PersonalExpenseTracker/src/components/darkMode.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { SunIcon, MoonIcon } from '@heroicons/react/24/solid'; // Example icons
 
 const DarkModeToggle = () => {
   const [darkMode, setDarkMode] = useState(()=>{
@@ -21,11 +20,11 @@ useEffect(()=>{
 
 
   return (
-    <div className="flex items-center mt-6">
+    <div className="flex items-center">
       <span className="mr-2 text-sm">{darkMode ? 'Dark Mode' : 'Light Mode'}</span>
       <div
         onClick={toggleDarkMode}
-        className={`w-10 h-5 flex items-center bg-gray-500 rounded-full p-1 cursor-pointer ${darkMode ? 'bg-gray-600' : 'bg-lightGreen'}`}
+        className={`w-10 h-5 flex items-center bg-gray-500 rounded-full p-1 cursor-pointer ${darkMode ? 'bg-gray-600' : 'bg-gray-500'}`}
       >
         <div
           className={`w-4 h-4 bg-white rounded-full shadow-md transform ${darkMode ? 'translate-x-5' : 'translate-x-0'}`}
